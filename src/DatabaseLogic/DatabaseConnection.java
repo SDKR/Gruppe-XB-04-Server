7c38c0f794f2a45b416e0e2b299e8fdc151dd3c2
Risgn94 <asger.thyregod@gmail.com>
2014-11-25 15:33:22 +0100

    Lidt oprydning i koden, og en færdig database funktion til folket!
    <3 Peace, love & harmony <3 Not. Dette er ikke RUC

--- src/DatabaseLogic/DatabaseConnection.java
+++ src/DatabaseLogic/DatabaseConnection.java
@@ -7,12 +7,14 @@ import java.util.List;
 
 import javax.swing.JOptionPane;
 
+import model.QueryBuild.QueryBuilder;
 import keyKeeper.*;
 
 public class DatabaseConnection {
 	
 	keyKeeper.KeyGetter GK = new keyKeeper.KeyGetter();
 	KeyChest KC = new KeyChest();
+	QueryBuilder QB = new QueryBuilder();
 
 	//Creates the needed information to connect to the database
 	
@@ -43,16 +45,17 @@ public class DatabaseConnection {
 		try {
 			doUpdate("Delete from cbscalendar.events where customevent = 1");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public void addingCBSCalendarToDB(String type, String location, String start, String end, String name, String text)
+	public void addingCBSCalendarToDB(String type, String location, String start, String end, String name, String text) throws SQLException
 	{
 		//Her skal være 2 switches til at bestemme Hvilken calendar event tilhører, og hvilken lokation.
 		int locationID = determineLocationID(location);
 		int calendarID = determineCalendarID(type);
 		int typeID = determineTypeID(type);
+		if(!start.contains("9-31") && !end.contains("9-31") )
+		{
 		try
 		{
 			
@@ -61,9 +64,22 @@ public class DatabaseConnection {
 			conn.close();
 			stmt.close();
 		}
+		
 		catch(SQLException e)
 		{
 			e.printStackTrace();
+			if(e.getErrorCode()== 1292)
+			{
+				throw new SQLException("Date does not exists");
+			}
+			//Else throw normal exception
+			else {
+				throw e;
+			}
+		}}
+		else
+		{
+			System.out.println("This date does not exists");
 		}
 	}
 	private int determineTypeID(String type) {
@@ -232,247 +248,11 @@ public class DatabaseConnection {
 		}
 		return passwordChecker;
 	}
-	//Creates a public String, which recieves 1 String input
-	public String getAdminID(String username) {
-		//Create a String an sets to nothing
-		String AdminID = "";
-		try {
-			//Get connection to database
-			getConnection();
-			//Creates a method, which allow us to use resultset, and recieve data from the database
-			stmt = conn.createStatement();
-			//Executes a query to recieve data
-			rs = stmt.executeQuery("select AdminID from BtcDatabase.Login_Information where CBSMail='"
-					+ username + "';");
-			//Sets the string equals the resultset
-			while (rs.next()) {
-				AdminID = rs.getString("AdminID");
-			}
-			//Close the connection to the database
-			closeConnection();
-			//If error, print exception in a dialogbox
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null,
-					e.getMessage());
-		}
-		//returns the string AdminID to be used later
-		return AdminID;
-	}
-
-	
-	
-	//Creates a public method, which revieves 2 inputs, and returns a string
-	public String authenticate(String userName, String password) {
-		//Creates an empty String
-		String ID = "";
-		try {
-			//Connects to database
-			getConnection();
-			//Execute query
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select * from BtcDatabase.Login_Information where CBSMail='"
-					+ userName + "' and Password='" + password + "';");
-			//Sets empty string equals resultset
-			while (rs.next()) {
-				ID = rs.getString("AdminID");
-			}
-			//Close connection to database
-			closeConnection();
-			//Prints Exception in a dialogbox
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null,
-					e.getMessage());
-		}
-		//returns the empty string which now contains the resultset given, by the query
-		return ID;
-		
-	}
-	//Creates a public method which returns a boolean, and recieves 3 input
-	public boolean creatingNewUserLogin(String userName, String newPassword,
-			String repeatPassword) {
-		//Sets boolean which will be returned to false
-		boolean userChecker = false;
-		try {
-			//Connects to database
-			getConnection();
-			//Creates an empty string
-			String testBruger = "";
-			//Executes a query which gives us all usernames in a string seperated by space
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select * from BtcDatabase.Login_Information");
-			while (rs.next()) {
-				testBruger += rs.getString("CBSMail");
-				testBruger += " ";
-			}
-			//If the entered passwords are not the same, print it, and return false 
-			if (!newPassword.equals(repeatPassword)) {
-				JOptionPane.showMessageDialog(null,
-						"The passwords are not the same", "alert",
-						JOptionPane.ERROR_MESSAGE);
-				return false;
-				//If the user which is trying to be created allready exists, return false
-			} else if (testBruger.contains(userName + " ")) {
-				JOptionPane.showMessageDialog(null,
-						"The Username does already exists", "alert",
-						JOptionPane.ERROR_MESSAGE);
-				return false;
-				//Else create user and return true
-			} else {
-				doUpdate("Insert Into BtcDatabase.Login_Information (CBSMail, Password, Balance, AdminID) VALUES ('"
-						+ userName
-						+ "', '"
-						+ newPassword
-						+ "', '"
-						+ 00.00
-						+ "', '" + 2 + "');");
-				JOptionPane.showMessageDialog(null, "The user " + userName
-						+ " has been created!", "Succes!",
-						JOptionPane.INFORMATION_MESSAGE);
-				userChecker = true;
-			}
-			//Close connection to database
-			closeConnection();
-			//Prints exception in dialogbox
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null,
-					e.getMessage());
-		}
-		return userChecker;
-	}
-
-	//Creates method which returns a boolean, and recieves one input
-	public void deleteUser(String username) {
-		//boolean userChecker = false;
-		int userChecker = 0;
-		try {
-			//Establish connection, and executes query
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select * from BtcDatabase.Login_Information where CBSMail = '"
-					+ username + "';");
-			//If query returns anything set boolean equals true
-			while(rs.next())
-			{
-				userChecker++;
-				//userChecker = true;
-			}
-			//If boolean equals true delete user from database
-			if(userChecker > 0)
-			{
-			doUpdate("DELETE FROM BtcDatabase.Login_Information where CBSMail='"
-					+ username + "';");
-			JOptionPane.showMessageDialog(null, "The user '" + username
-					+ "'has been deleted!", "alert", JOptionPane.INFORMATION_MESSAGE);
-			}
-			//Else user cant be created, since it does not exists
-			else
-			{
-				JOptionPane.showMessageDialog(null, "The user '" + username
-						+ "' does not exist!", "alert", JOptionPane.ERROR_MESSAGE);
-			}
-			//Prints exception in dialogbox
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null,
-					e.getMessage());
-		}
-	}
-	
-
-	//Creates a method which returns a boolean, and recieves 3 inputs
-	public boolean authenticatePassword (String username, String newPassword, String oldPassword)
-	{
-		//Creates a boolean and sets to false
-		boolean authenticatePassword = false;
-		//Creates an empty string
-		String passwordCheck = "";
-		try
-		{
-		//Establish connection and executes query
-		getConnection();
-		stmt = conn.createStatement();
-		rs = stmt.executeQuery("select * from btcDatabase.Login_Information where CBSMail = '"
-				+username+ "' and password = '"+oldPassword+"'");
-		//Sets the empty string to the resultset, and set boolean equals true, if the resultset returns anything
-		while(rs.next())
-		{
-			passwordCheck = rs.getString("balance");
-			authenticatePassword = true;
-		}
-		//If boolean equals true, do the database update
-		if(authenticatePassword = true)
-		{
-			doUpdate("UPDATE btcDatabase.Login_Information SET password = '"
-						+newPassword+"' WHERE CBSMail = '"
-						+username+"'");
-		}
-		//Else print the original password is incorrect
-		else
-		{
-			JOptionPane.showMessageDialog(null, "The original password is incorrect",
-					"alert", JOptionPane.ERROR_MESSAGE);
-		}
-		
-		}
-		//Prints exception in a dialogbox
-		catch(SQLException e)
-		{
-			JOptionPane.showMessageDialog(null,
-					e.getMessage());
-		}
-		//return the boolean
-		return authenticatePassword;
-	}
-	/*************************
-	 * FOR LOOP FOR JTABLE *
-	 ************************/
-	
 	
 	/*************************
 	 * Arraylist to bugtable *
 	 ************************/
 	//Creates a method returning a array list, and receives nothing
-	public ArrayList<String> arrayUserEmail()
-	{
-		//Creates an empty arraylist
-		ArrayList<String> userEmail = new ArrayList<String>();
-		try {
-			//Establish connection to database
-			getConnection();
-			//Executes query
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select email from cbscalendar.users");
-			//Adds every object from the resultset to the empty arraylist
-			while (rs.next()) {
-				userEmail.add(rs.getString("email"));
-				
-			}
-			//Close connection
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		//Returns the arraylist
-		return userEmail;
-	}
-	
-	public ArrayList<String> arrayBugText()
-	{
-		ArrayList<String> bugText = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select bugText from btcdatabase.bugTable");
-			while (rs.next()) {
-				bugText.add(rs.getString("bugText"));
-				
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return bugText;
-	}
-	
 	public String[][] arrayID() {
 		String[] headerNames = {"userid", "email", "active", "created", "password", "Admin"};  
 		int rowCounter = 0;
@@ -513,241 +293,7 @@ public class DatabaseConnection {
 		}
 		return doubleArray;
 	}
-
-	public ArrayList<String> AdminID() {
-		
-		ArrayList<String> AdminID = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select AdminID from btcdatabase.login_information");
-			while (rs.next()) {
-				AdminID.add(rs.getString("adminID"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return AdminID;
-	}
-
-	public ArrayList<String> password() {
-		ArrayList<String> password = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select password from btcdatabase.login_information");
-			while (rs.next()) {
-				password.add(rs.getString("password"));
-			}          
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return password;
-	}
 	
-	/****************************************************
-	 * Methods which return all info about all transfers*
-	 ***************************************************/
-	
-	public ArrayList<String> TransferID() {
-		ArrayList<String> TransferID = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select transferID from btcdatabase.Transfer_information");
-			while (rs.next()) {
-				TransferID.add(rs.getString("transferID"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return TransferID;
-	}
-	
-	public ArrayList<String> Date() {
-		ArrayList<String> Date = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select date from btcdatabase.Transfer_information");
-			while (rs.next()) {
-				Date.add(rs.getString("date"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return Date;
-	}
-	
-	public ArrayList<String> outgoingID() {
-		ArrayList<String> outgoingID = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select outgoing from btcdatabase.Transfer_information");
-			while (rs.next()) {
-				outgoingID.add(rs.getString("outgoing"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return outgoingID;
-	}
-	
-	public ArrayList<String> ingoingID() {
-		ArrayList<String> ingoing = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select ingoing from btcdatabase.Transfer_information");
-			while (rs.next()) {
-				ingoing.add(rs.getString("ingoing"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return ingoing;
-	}
-	
-	public ArrayList<String> amount() {
-		ArrayList<String> amount = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select amount from btcdatabase.Transfer_information");
-			while (rs.next()) {
-				amount.add(rs.getString("amount"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return amount;
-	}
-	
-	public ArrayList<String> tUserID() {
-		ArrayList<String> tUserID = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select UserID from btcdatabase.Transfer_information");
-			while (rs.next()) {
-				tUserID.add(rs.getString("UserID"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return tUserID;
-	}
-	
-	/****************************************************
-	 * Methods which return all info about user transfers*
-	 ***************************************************/
-	
-	public ArrayList<String> uTransferID(String username) {
-		ArrayList<String> uTransferID = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select transferID from btcdatabase.Transfer_information where ingoing = '"+username+"' or outgoing = '"+username+"'");
-			while (rs.next()) {
-				uTransferID.add(rs.getString("transferID"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return uTransferID;
-	}
-	
-	public ArrayList<String> uDate(String username) {
-		ArrayList<String> uDate = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select Date from btcdatabase.Transfer_information where ingoing = '"+username+"' or outgoing = '"+username+"'");
-			while (rs.next()) {
-				uDate.add(rs.getString("Date"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return uDate;
-	}
-	
-	public ArrayList<String> uOutgoing(String username) {
-		ArrayList<String> uOutgoing = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select outgoing from btcdatabase.Transfer_information where ingoing = '"+username+"' or outgoing = '"+username+"'");
-			while (rs.next()) {
-				uOutgoing.add(rs.getString("outgoing"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return uOutgoing;
-	}
-	
-	public ArrayList<String> uIngoing(String username) {
-		ArrayList<String> uIngoing = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select Ingoing from btcdatabase.Transfer_information where ingoing = '"+username+"' or outgoing = '"+username+"'");
-			while (rs.next()) {
-				uIngoing.add(rs.getString("Ingoing"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return uIngoing;
-	}
-	
-	public ArrayList<String> uAmount(String username) {
-		ArrayList<String> uAmount = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select Amount from btcdatabase.Transfer_information where ingoing = '"+username+"' or outgoing = '"+username+"'");
-			while (rs.next()) {
-				uAmount.add(rs.getString("Amount"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return uAmount;
-	}
-	
-	public ArrayList<String> uID(String username) {
-		ArrayList<String> uID = new ArrayList<String>();
-		try {
-			getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select UserID from btcdatabase.Transfer_information where ingoing = '"+username+"' or outgoing = '"+username+"'");
-			while (rs.next()) {
-				uID.add(rs.getString("UserID"));
-			}
-			closeConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return uID;
-	}
 	//Creates a method which returns a int, and recieves one input
 	public int doUpdate(String Update) throws SQLException {
 		//Establish connection to database
@@ -860,10 +406,4 @@ public class DatabaseConnection {
 	public void setSqlPasswd(String sqlPasswd) {
 		this.sqlPasswd = sqlPasswd;
 	}
-
-	
-
-	
-	
-
 }
--- src/model/calendar/GetCalendarData.java
+++ src/model/calendar/GetCalendarData.java
@@ -43,6 +43,7 @@ public class GetCalendarData {
      * Allows client to retrieve CBS's calendar and then access it.
      * @throws Exception
      */
+    private static int arrayRows; 
     public void getDataFromCalendar() throws Exception {
     	
     	Gson gson1 = new GsonBuilder().create();
@@ -55,27 +56,18 @@ public class GetCalendarData {
     	String userID = "asth13ac";
         String json = readUrl("http://calendar.cbs.dk/events.php/"+userID+"/"+"92a62decac8a6524ec6c382a0ca27419"+".json");
     	Events event = gson1.fromJson(json, Events.class);
-    	
-    	
-    	
-    	System.out.println(event);
-        
-        System.out.println(json);
-        System.out.println(event.getEvents().get(1).getActivityid());
-        System.out.println(event.getEvents().get(1).getCreatedby());
-        System.out.println(event.getEvents().get(1).getDescription());
-        System.out.println(event.getEvents().get(1).getStart().get(0));
-        int rows = event.getEvents().size();
+    
+    	int rCount = 0;
         DC.clearOldCBSData();
-        for(int rCount = 0 ; rCount < rows ; rCount++)
+        setArrayRows(event.getEvents().size()); 
+        while(rCount < arrayRows) 
         {
-        	System.out.println(rCount);
         	String startTime = startTimeToString(event.getEvents().get(rCount).getStart());
         	String endTime = endTimeToString(event.getEvents().get(rCount).getEnd());
         	DC.addingCBSCalendarToDB(event.getEvents().get(rCount).getType(), event.getEvents().get(rCount).getLocation(), startTime, endTime, event.getEvents().get(rCount).getDescription(), "Et eller andet");
+        	rCount++;
         }
     }
-    
     private String endTimeToString(ArrayList<String> end) {
 		String stringToBeReturned = (end.get(0)+"-"+end.get(1)+"-"+end.get(2)+" "+end.get(3)+":"+end.get(4)+":00");
 		return stringToBeReturned;
@@ -89,13 +81,11 @@ public class GetCalendarData {
 		GetCalendarData GCD = new GetCalendarData();
 		try {
 			GCD.getDataFromCalendar();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			if(e.getErrorCode() == 1292)
-			{
-				
-			}
-			
-		}
+		} catch (Exception e) {
+			e.printStackTrace();		
+	}}
+
+	public static void setArrayRows(int arrayRows) {
+		GetCalendarData.arrayRows = arrayRows;
 	}
 	}
