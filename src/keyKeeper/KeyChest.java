b769bd039acaeaf0156e367dd926c95b852e711b
Risgn94 <asger.thyregod@gmail.com>
2014-11-25 16:17:37 +0100

    En start på noget kryptering
    Til señor Emilio Hernandez

--- src/keyKeeper/KeyChest.java
+++ src/keyKeeper/KeyChest.java
@@ -3,9 +3,12 @@ package keyKeeper;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import Encryption.encryption;
+
 public class KeyChest {
 
 	keyKeeper.KeyGetter GK = new keyKeeper.KeyGetter();
+	
 	
 	private String sqlUser;
 	private String sqlPasswd;
@@ -25,15 +28,29 @@ public class KeyChest {
 		portNr = GK.getCompleteFile().get(4);
 		
 	}
+	public static void main (String []args)
+	{
+		encryption cryp = new encryption();
+		keyKeeper.KeyGetter GK = new keyKeeper.KeyGetter();
+		
+		GK.getConfigInfo();
+		String stringToCryp = GK.getCompleteFile().get(1);
+		System.out.println(stringToCryp);
+		
+		String emilCryp = cryp.xor_decrypt("emil", "458");
+		System.out.println(emilCryp);
+		System.out.println(cryp.xor_decrypt(emilCryp, "458"));
+		
+		
+	}
+	
 	
 //	public void kryptering(){
 //	JEG HADER LOOPS!?!#"¤! 
 //		public void decrypt(byte[] b)
 	
-		public void decrypt()
+		/*public void decrypt()
 		{
-			
-			
 //		Størrelse på arraylist (5)
 		int aSize = GK.getCompleteFile().size();
 		int ii = 0;
@@ -62,7 +79,7 @@ public class KeyChest {
       while (printlist.hasNext()) {
       System.out.println(printlist.next());
       }
-		}
+	  }*/
 //	}
 		
 	public ArrayList<String> getdecryptFile() {
